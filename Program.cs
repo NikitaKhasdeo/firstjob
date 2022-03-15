055557c8fe67ba7b95f7b8b525c4f62c6cb634f3
NikitaKhasdeo <101544635+NikitaKhasdeo@users.noreply.github.com>
2022-03-14 21:48:33 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Variables";
+
+            char letter = 'D';
+            int number = 100;
+            float weight = 98.5f;
+            double pi = 3.14;
+            decimal sum = 1000.0m;
+            string name = "Dhamukanna";
+            bool IsActive = true;
+
+            Console.WriteLine("char Letter :\t" + letter);
+            Console.WriteLine("int Number :\t" + number);
+            Console.WriteLine("float Weight :\t" + weight);
+            Console.WriteLine("double Pi :\t" + pi);
+            Console.WriteLine("decimal Sum :\t" + sum);
+            Console.WriteLine("string Name :\t" + name);
+            Console.WriteLine("bool Status :\t" + IsActive);
+            Console.ReadLine();
+
+                
+
+        }
+    }
+}
